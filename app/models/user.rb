eead9dff9f385904cf209498f495ef026dff7880
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-25 14:05:52 +0900

    ✨ADD:authenticationモデルと関連リレーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  has_many :authentications, dependent: :destroy
+  accepts_nested_attributes_for :authentications
 
   validates:email, presence: true, uniqueness: true
   validates:name, presence: true
