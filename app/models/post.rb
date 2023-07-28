96403287df5981ba197a06f223f5bdfef4043c20
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-28 11:02:24 +0900

    ✨ADD:OGPスクレイピング機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  require 'nokogiri'
+  require 'open-uri'
+
   belongs_to :category
   has_many :comments, dependent: :destroy
 
@@ -8,4 +11,42 @@ class Post < ApplicationRecord
   validates :app_url, presence: true
   validates :github_url, presence: true
   validates :category_id, presence: true
+
+  attr_accessor :site_description, :site_image
+
+  def fetch_ogp_info
+    url = app_url
+    charset = nil
+    begin
+      html = URI.open(url) do |f|
+        charset = f.charset
+        f.read
+      end
+      Nokogiri::HTML.parse(html, charset)
+    rescue OpenURI::HTTPError => e
+      Rails.logger.error "Failed to fetch OGP info for #{url}: #{e}"
+      nil
+    rescue SocketError => e
+      Rails.logger.error "Failed to open TCP connection for #{url}: #{e}"
+      nil
+    end
+  end
+
+  # def site_discription
+  #   doc = fetch_ogp_info
+  #   if doc.css('//meta[property="og:description"]/@content').empty?
+  #     doc.css('//meta[name$="escription"]/@content').to_s
+  #   else
+  #     doc.css('//meta[property="og:description"]/@content').to_s
+  #   end
+  # end
+
+  def site_image
+    doc = fetch_ogp_info
+    if doc
+      doc.css('//meta[property="og:image"]/@content').to_s
+    else
+      return ""
+    end
+  end
 end
