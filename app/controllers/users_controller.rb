bfe7cc31c0d711299f57703f53a8c108efb553cf
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-30 10:37:35 +0900

    🐛FIX:require_login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(current_user.id)
-  end
 
   def edit
     @user = User.find(current_user.id)
