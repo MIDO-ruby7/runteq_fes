8b1b7d780aeb77826b2d622899b782f8b61ff65b
Midori Takahashi <mintgreen7programming@gmail.com>
2023-08-03 18:19:22 +0900

    FIX:graduating_classのバリデーションを削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,6 @@ class Post < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy
 
-  validates :graduating_class, presence: true
   validates :name, presence: true
   validates :app_name, presence: true
   validates :app_url, presence: true
